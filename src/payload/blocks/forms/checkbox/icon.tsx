d3f8ec977deb6565e7175c1dfa562b73dc4a543c
Michael Owuor <michael.owuor@gmail.com>
2025-11-09 18:47:47 +0300

    style: changed h-4 w-4 to size-4

--- src/payload/blocks/forms/checkbox/icon.tsx
+++ src/payload/blocks/forms/checkbox/icon.tsx
@@ -16,7 +16,7 @@ const Checkbox = forwardRef<HTMLButtonElement, CheckboxProps>(({ className, ...p
 			// peer class is added for easier styling of sibling elements when the checkbox state changes
 			// defines default size, shape, and focus ring styling
 			// applies primary color styles when the checkbox is in the 'checked' state
-			"peer border-primary ring-offset-background focus-visible:ring-ring data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground h-4 w-4 shrink-0 rounded border focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
+			"peer border-primary ring-offset-background focus-visible:ring-ring data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground size-4 shrink-0 rounded border focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
 			className,
 		)}
 		ref={ref}
